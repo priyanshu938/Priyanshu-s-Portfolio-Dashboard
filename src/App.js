20c184ac429c65f23f28780828d6f08cad6a222b
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2022-07-31 00:42:44 +0530

    folder setup for dashboard

--- src/App.js
+++ src/App.js
@@ -4,8 +4,16 @@ import Login from "./components/Login/Login";
 import Dashboard from "./components/Dashboard/Dashboard";
 
 function App() {
-  const [isLoggedIn, setIsLoggedIn] = useState(false);
-  return <div>{!isLoggedIn ? <Login setIsLoggedIn={setIsLoggedIn}/> : <Dashboard setIsLoggedIn={setIsLoggedIn}/>}</div>;
+  const [isLoggedIn, setIsLoggedIn] = useState(true); //change it to false later to view login screen first
+  return (
+    <div>
+      {!isLoggedIn ? (
+        <Login setIsLoggedIn={setIsLoggedIn} />
+      ) : (
+        <Dashboard setIsLoggedIn={setIsLoggedIn} />
+      )}
+    </div>
+  );
 }
 
 export default App;
--- src/components/Dashboard/Dashboard.js
+++ src/components/Dashboard/Dashboard.js
@@ -1,11 +1,43 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Snackbar from "../ReusableComponents/Snackbar";
 
-const Dashboard = ({setIsLoggedIn}) => {
+const Dashboard = ({ setIsLoggedIn }) => {
+  //auto log out after 1 hour code
+  {
+    /*const [sessionExpireTimeLeft, setSessionExpireTimeLeft] = useState(3600);
+  useEffect(() => {
+    if (sessionExpireTimeLeft === 0) setIsLoggedIn(false);
+    const timer =
+      sessionExpireTimeLeft > 0 &&
+      setInterval(
+        () => setSessionExpireTimeLeft(sessionExpireTimeLeft - 1),
+        1000
+      );
+    return () => clearInterval(timer);
+  }, [sessionExpireTimeLeft]); */
+  }
+
+  //image list mui for grid like all things
+  const [loginSuccessful, setLoginSuccessful] = useState(true);
+
   return (
     <div>
-    <button onClick={()=>setIsLoggedIn(false)}>Log out</button>
-    </div>
-  )
-}
+      <Snackbar
+        isOpen={loginSuccessful}
+        severity="success"
+        message="Successfully logged in"
+        setIsOpen={setLoginSuccessful}
+      />
+      <h1>Dashboard</h1>
+      <div>Skills</div>
+      <div>Skills</div>
+      <div>Skills</div>
+      <div>Skills</div>
+      <div>Skills</div>
 
-export default Dashboard
+      <button onClick={() => setIsLoggedIn(false)}>Log out</button>
+    </div>
+  );
+};
+
+export default Dashboard;
