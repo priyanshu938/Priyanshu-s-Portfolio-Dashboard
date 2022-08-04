f32b8cb869d34409f0a3502f89d2bf36846499dd
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2022-08-04 17:48:04 +0530

    loader added

--- src/components/Dashboard/About Me/AboutMe.js
+++ src/components/Dashboard/About Me/AboutMe.js
@@ -6,6 +6,7 @@ import EditIcon from "@mui/icons-material/Edit";
 import LaunchIcon from "@mui/icons-material/Launch";
 import url from "../../../ServerUrl";
 import Snackbar from "../../ReusableComponents/Snackbar";
+import Spinner from "../../ReusableComponents/Spinner";
 
 const AboutMe = () => {
   const [myDetails, setMyDetails] = useState([]);
@@ -17,8 +18,10 @@ const AboutMe = () => {
   const [image, setImage] = useState("");
   const [description, setDescription] = useState("");
   const [link, setLink] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
 
   const getMyDetails = async () => {
+    setIsLoading(true);
     const response = await fetch(`${url}/resume/getResume`, {
       method: "GET",
     });
@@ -29,108 +32,115 @@ const AboutMe = () => {
       setImage(json.result[0].image);
       setDescription(json.result[0].description);
       setLink(json.result[0].link);
+      setIsLoading(false);
     }
   };
 
   useEffect(() => getMyDetails, [isOpenSnackbar]);
 
   return (
-    <div className="mt-4">
-      <Snackbar
-        isOpen={isOpenSnackbar}
-        severity={severity}
-        message={message}
-        setIsOpen={setIsOpenSnackbar}
-      />
-      {openEditAboutMeModal && (
-        <EditAboutMeModal
-          openEditAboutMeModal={openEditAboutMeModal}
-          setOpenEditAboutMeModal={setOpenEditAboutMeModal}
-          setIsOpenSnackbar={setIsOpenSnackbar}
-          setSeverity={setSeverity}
-          setMessage={setMessage}
-          id={id}
-          image={image}
-          description={description}
-          link={link}
-        />
-      )}
-      <Typography px={2} variant="h4" component="div" gutterBottom>
-        About Me
-      </Typography>
-      <div className="mt-5 container">
-        <div className="row">
-          <div className="col-4">
-            <img
-              src={myDetails.image}
-              alt="not found"
-              style={{ height: "50vh" }}
+    <div>
+      {isLoading ? (
+        <Spinner />
+      ) : (
+        <div className="mt-4">
+          <Snackbar
+            isOpen={isOpenSnackbar}
+            severity={severity}
+            message={message}
+            setIsOpen={setIsOpenSnackbar}
+          />
+          {openEditAboutMeModal && (
+            <EditAboutMeModal
+              openEditAboutMeModal={openEditAboutMeModal}
+              setOpenEditAboutMeModal={setOpenEditAboutMeModal}
+              setIsOpenSnackbar={setIsOpenSnackbar}
+              setSeverity={setSeverity}
+              setMessage={setMessage}
+              id={id}
+              image={image}
+              description={description}
+              link={link}
             />
-          </div>
-          <div className="col">
-            <div className="mt-4 mb-5">
-              <label
-                className="ms-2 "
-                style={{ color: "#00b7eb", fontSize: "20px" }}
-              >
-                Name :
-              </label>
-              <br />
-              <TextField
-                id="standard-basic"
-                variant="standard"
-                style={{ width: "40vw" }}
-                type="text"
-                className="ms-2"
-                value="Hello, I am 
-              Priyanshu Tiwari"
-              />
-              <br />
-              <label
-                className="ms-2 mt-4 "
-                style={{ color: "#00b7eb", fontSize: "20px" }}
-              >
-                Description :
-              </label>
-              <br />
-              <TextField
-                id="standard-basic"
-                variant="standard"
-                style={{ width: "40vw" }}
-                multiline
-                type="text"
-                className="ms-2 mb-5 "
-                value={myDetails.description}
-              />
-              <br />
-              <Link href={myDetails.link} target="_blank" underline="none">
-                <Button
-                  className=""
-                  variant="contained"
-                  endIcon={<LaunchIcon />}
-                >
-                  Resume Link
-                </Button>
-              </Link>
-            </div>
-          </div>
-          <div className="row">
-            <div className="col-4"></div>
-            <div className=" col">
-              <Button
-                variant="contained"
-                startIcon={<EditIcon />}
-                style={{ backgroundColor: "gray", width: "20vw" }}
-                onClick={() => {
-                  setOpenEditAboutMeModal(true);
-                }}
-              >
-                Edit
-              </Button>
+          )}
+          <Typography px={2} variant="h4" component="div" gutterBottom>
+            About Me
+          </Typography>
+          <div className="mt-5 container">
+            <div className="row">
+              <div className="col-4">
+                <img
+                  src={myDetails.image}
+                  alt="not found"
+                  style={{ height: "50vh" }}
+                />
+              </div>
+              <div className="col">
+                <div className="mt-4 mb-5">
+                  <label
+                    className="ms-2 "
+                    style={{ color: "#00b7eb", fontSize: "20px" }}
+                  >
+                    Name :
+                  </label>
+                  <br />
+                  <TextField
+                    id="standard-basic"
+                    variant="standard"
+                    style={{ width: "40vw" }}
+                    type="text"
+                    className="ms-2"
+                    value="Hello, I am 
+            Priyanshu Tiwari"
+                  />
+                  <br />
+                  <label
+                    className="ms-2 mt-4 "
+                    style={{ color: "#00b7eb", fontSize: "20px" }}
+                  >
+                    Description :
+                  </label>
+                  <br />
+                  <TextField
+                    id="standard-basic"
+                    variant="standard"
+                    style={{ width: "40vw" }}
+                    multiline
+                    type="text"
+                    className="ms-2 mb-5 "
+                    value={myDetails.description}
+                  />
+                  <br />
+                  <Link href={myDetails.link} target="_blank" underline="none">
+                    <Button
+                      className=""
+                      variant="contained"
+                      endIcon={<LaunchIcon />}
+                    >
+                      Resume Link
+                    </Button>
+                  </Link>
+                </div>
+              </div>
+              <div className="row">
+                <div className="col-4"></div>
+                <div className=" col">
+                  <Button
+                    variant="contained"
+                    startIcon={<EditIcon />}
+                    style={{ backgroundColor: "gray", width: "20vw" }}
+                    onClick={() => {
+                      setOpenEditAboutMeModal(true);
+                    }}
+                  >
+                    Edit
+                  </Button>
+                </div>
+              </div>
             </div>
           </div>
         </div>
-      </div>
+      )}
     </div>
   );
 };
