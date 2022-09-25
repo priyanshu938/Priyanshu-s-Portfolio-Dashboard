466546aa0be1ed6431f5ced0eb521f1d12ec9cc1
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2022-09-25 23:30:12 +0530

    +1

--- src/ServerUrl.js
+++ src/ServerUrl.js
@@ -1,3 +1,3 @@
-const url="https://priyanshu-portfolio-backend.herokuapp.com"
+const url="https://priyanshu-portfolio-backend.up.railway.app"
 // const url="http://localhost:8080"
 export default url;
