e1fa072491ecc1ee09f677533d5f9fdc80e9f117
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2022-12-30 21:52:17 +0530

    chatbot integration completed

--- src/components/Dashboard/Chatbot.js
+++ src/components/Dashboard/Chatbot.js
@@ -4,73 +4,139 @@ import url from "../../ServerUrl";
 import Input from "@mui/material/Input";
 import SendIcon from "@mui/icons-material/Send";
 import InputAdornment from "@mui/material/InputAdornment";
-import Spinner from "../ReusableComponents/Spinner";
+import SnackbarForChatbot from "../ReusableComponents/SnackbarForChatbot";
+import Snackbar from "../ReusableComponents/Snackbar";
+import ContentPasteOutlinedIcon from "@mui/icons-material/ContentPasteOutlined";
+import CodeEditor from "@uiw/react-textarea-code-editor";
 
 const Chatbot = () => {
-  const [isLoading, setIsLoading] = useState(false);
   const [message, setMessage] = useState("");
   const [chatbotResponse, setChatbotResponse] = useState("");
+  const [severity, setSeverity] = useState("");
+  const [messageSnackbar, setMessageSnackbar] = useState("");
+  const [isOpen, setIsOpen] = useState(false);
+  const [isOpenSnackbarChatbot, setIsOpenSnackbarChatbot] = useState(false);
+
   const body = { prompt: message };
+
   const handleChange = (event) => {
     setMessage(event.target.value);
   };
 
   const handleSubmit = async (event) => {
-    event.preventDefault();
-    setIsLoading(true);
-    const response = await fetch(`${url}/chatbot/getResponseFromChatbot`, {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-        "auth-token": window.localStorage.getItem("token"),
-      },
-      body: JSON.stringify(body),
-    });
-    const json = await response.json();
-    setIsLoading(false);
-    setChatbotResponse(json.message);
+    if (message.length > 0) {
+      event.preventDefault();
+      setIsOpenSnackbarChatbot(true);
+
+      try {
+        const response = await fetch(`${url}/chatbot/getResponseFromChatbot`, {
+          method: "POST",
+          headers: {
+            "Content-Type": "application/json",
+            "auth-token": window.localStorage.getItem("token"),
+          },
+          body: JSON.stringify(body),
+        });
+        const json = await response.json();
+        if (response.status === 200) {
+          setChatbotResponse(json.message);
+          setIsOpenSnackbarChatbot(false);
+        } else {
+          setIsOpen(true);
+          setMessageSnackbar("Something went wrong...Try again later!");
+          setSeverity("error");
+        }
+      } catch (error) {
+        setIsOpen(true);
+        setMessageSnackbar("Something went wrong...Try again later!");
+        setSeverity("error");
+      }
+    } else {
+      setIsOpen(true);
+      setMessageSnackbar("Please enter some text!");
+      setSeverity("warning");
+    }
   };
   return (
-    <div>
-      {isLoading ? (
-        <Spinner />
-      ) : (
-        <div className="mt-4 mx-4">
-          <Typography px={2} variant="h4" component="div" gutterBottom>
-            My AI Assistant
-          </Typography>
-          <form
-            onSubmit={handleSubmit}
-            noValidate
-            autoComplete="off"
-            style={{ height: "50vh", width: "50vw" }}
-            className="mx-4 mt-4"
-          >
-            <Input
-              id="input-with-icon-adornment"
-              placeholder="Hi Priyanshu, How can I help you?"
-              sx={{ width: "100%" }}
-              endAdornment={
-                <InputAdornment position="end">
-                  <SendIcon />
-                </InputAdornment>
-              }
-              value={message}
-              onChange={handleChange}
-            />
-            {chatbotResponse && (
-              <Typography
-                variant="h6"
-                component="div"
-                gutterBottom
-                className="mt-4"
+    <div className="mt-4 mx-4">
+      <Snackbar
+        isOpen={isOpen}
+        severity={severity}
+        message={messageSnackbar}
+        setIsOpen={setIsOpen}
+      />
+      <SnackbarForChatbot
+        isOpen={isOpenSnackbarChatbot}
+        severity="info"
+        message="Fetching results..."
+        setIsOpen={setIsOpenSnackbarChatbot}
+      />
+      <Typography px={2} variant="h4" component="div" gutterBottom>
+        My AI Assistant
+      </Typography>
+      <form
+        onSubmit={handleSubmit}
+        noValidate
+        autoComplete="off"
+        style={{ height: "50vh", width: "60vw" }}
+        className="mx-4 mt-4"
+      >
+        <Input
+          id="input-with-icon-adornment"
+          placeholder="Hi Sir! How can I help you?"
+          sx={{ width: "100%" }}
+          endAdornment={
+            <InputAdornment position="end" style={{ cursor: "pointer" }}>
+              <SendIcon onClick={handleSubmit} />
+            </InputAdornment>
+          }
+          value={message}
+          onChange={handleChange}
+          required
+        />
+        {chatbotResponse && (
+          <div className="mt-4 ">
+            <div className="d-flex flex-row-reverse">
+              <b
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  navigator.clipboard.writeText(chatbotResponse);
+                  setIsOpen(true);
+                  setMessageSnackbar("Copied to clipboard!");
+                  setSeverity("success");
+                }}
               >
-                Ans: {chatbotResponse}
-              </Typography>
-            )}
-          </form>
-        </div>
-      )}
+                Copy&nbsp;
+              </b>
+              <ContentPasteOutlinedIcon
+                style={{ cursor: "pointer" }}
+                onClick={() => {
+                  navigator.clipboard.writeText(chatbotResponse);
+                  setIsOpen(true);
+                  setMessageSnackbar("Copied to clipboard!");
+                  setSeverity("success");
+                }}
+              />
+            </div>
+            <CodeEditor
+              value={chatbotResponse}
+              language="java"
+              className="form-control"
+              padding={15}
+              style={{
+                backgroundColor: "black",
+                color: "white",
+                fontFamily: "monospace",
+                borderRadius: "4px",
+                fontSize: "20px",
+                height: "60vh",
+                overflowY: "scroll",
+              }}
+              readOnly
+            />
+          </div>
+        )}
+      </form>
     </div>
   );
 };
---
+++ src/components/ReusableComponents/SnackbarForChatbot.js
@@ -0,0 +1,33 @@
+import React from "react";
+import Stack from "@mui/material/Stack";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
+
+const Alert = React.forwardRef(function Alert(props, ref) {
+  return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
+});
+const SnackbarForChatbot = ({ severity, message, isOpen, setIsOpen }) => {
+  const handleClose = (event, reason) => {
+    if (reason === "clickaway") {
+      return;
+    }
+    setIsOpen(!isOpen);
+  };
+  return (
+    <>
+      <Stack>
+        <Snackbar
+          anchorOrigin={{ vertical: "top", horizontal: "right" }}
+          open={isOpen}
+          onClose={handleClose}
+        >
+          <Alert severity={severity} sx={{ width: "100%" }}>
+            {message}
+          </Alert>
+        </Snackbar>
+      </Stack>
+    </>
+  );
+};
+
+export default SnackbarForChatbot;
