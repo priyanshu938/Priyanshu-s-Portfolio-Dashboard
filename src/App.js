283b4b651f91bd6a9b671b9bec24b9d44e463628
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-01-23 15:24:21 +0530

    dark switch added

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ import PageNotFound from "./PageNotFound";
 import { ThemeProvider, createTheme } from '@mui/material/styles';
 import CssBaseline from '@mui/material/CssBaseline';
 
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-  },
-});
+
 
 function App() {
+    const [darkMode, setDarkMode] = React.useState(true);
+    const darkTheme = createTheme({
+        palette: {
+          mode: darkMode===true?'dark':'light',
+        },
+      });
   return (
      <ThemeProvider theme={darkTheme}>
       <CssBaseline />
@@ -21,7 +23,7 @@ function App() {
       <Router>
         <Routes>
           <Route exact path="/" element={<Login />} />
-          <Route exact path="/dashboard" element={<Dashboard />} />
+          <Route exact path="/dashboard" element={<Dashboard darkMode={darkMode} setDarkMode={setDarkMode}/>} />
           <Route exact path="*" element={<PageNotFound />} />
         </Routes>
       </Router>
--- src/components/Dashboard/Sidebar.js
+++ src/components/Dashboard/Sidebar.js
@@ -36,6 +36,9 @@ import DialogContent from "@mui/material/DialogContent";
 import DialogContentText from "@mui/material/DialogContentText";
 import EmailIcon from "@mui/icons-material/Email";
 import SmartToyOutlinedIcon from "@mui/icons-material/SmartToyOutlined";
+import Switch from "@mui/material/Switch";
+import { useTheme } from '@mui/material/styles';
+
 
 const drawerWidth = 240;
 
@@ -103,13 +106,60 @@ const Drawer = styled(MuiDrawer, {
     "& .MuiDrawer-paper": closedMixin(theme),
   }),
 }));
+const MaterialUISwitch = styled(Switch)(({ theme }) => ({
+  width: 62,
+  height: 34,
+  padding: 7,
+  "& .MuiSwitch-switchBase": {
+    margin: 1,
+    padding: 0,
+    transform: "translateX(6px)",
+    "&.Mui-checked": {
+      color: "#fff",
+      transform: "translateX(22px)",
+      "& .MuiSwitch-thumb:before": {
+        backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+          "#fff"
+        )}" d="M4.2 2.5l-.7 1.8-1.8.7 1.8.7.7 1.8.6-1.8L6.7 5l-1.9-.7-.6-1.8zm15 8.3a6.7 6.7 0 11-6.6-6.6 5.8 5.8 0 006.6 6.6z"/></svg>')`,
+      },
+      "& + .MuiSwitch-track": {
+        opacity: 1,
+        backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+      },
+    },
+  },
+  "& .MuiSwitch-thumb": {
+    backgroundColor: theme.palette.mode === "dark" ? "#003892" : "#001e3c",
+    width: 32,
+    height: 32,
+    "&:before": {
+      content: "''",
+      position: "absolute",
+      width: "100%",
+      height: "100%",
+      left: 0,
+      top: 0,
+      backgroundRepeat: "no-repeat",
+      backgroundPosition: "center",
+      backgroundImage: `url('data:image/svg+xml;utf8,<svg xmlns="http://www.w3.org/2000/svg" height="20" width="20" viewBox="0 0 20 20"><path fill="${encodeURIComponent(
+        "#fff"
+      )}" d="M9.305 1.667V3.75h1.389V1.667h-1.39zm-4.707 1.95l-.982.982L5.09 6.072l.982-.982-1.473-1.473zm10.802 0L13.927 5.09l.982.982 1.473-1.473-.982-.982zM10 5.139a4.872 4.872 0 00-4.862 4.86A4.872 4.872 0 0010 14.862 4.872 4.872 0 0014.86 10 4.872 4.872 0 0010 5.139zm0 1.389A3.462 3.462 0 0113.471 10a3.462 3.462 0 01-3.473 3.472A3.462 3.462 0 016.527 10 3.462 3.462 0 0110 6.528zM1.665 9.305v1.39h2.083v-1.39H1.666zm14.583 0v1.39h2.084v-1.39h-2.084zM5.09 13.928L3.616 15.4l.982.982 1.473-1.473-.982-.982zm9.82 0l-.982.982 1.473 1.473.982-.982-1.473-1.473zM9.305 16.25v2.083h1.389V16.25h-1.39z"/></svg>')`,
+    },
+  },
+  "& .MuiSwitch-track": {
+    opacity: 1,
+    backgroundColor: theme.palette.mode === "dark" ? "#8796A5" : "#aab4be",
+    borderRadius: 20 / 2,
+  },
+}));
 
-export default function Sidebar2({ content, setContent }) {
+export default function Sidebar2({ content, setContent,darkMode,setDarkMode }) {
   let navigate = useNavigate();
+  const theme=useTheme();
 
   const [open, setOpen] = useState(true);
   const [openDialog, setOpenDialog] = useState(false);
-  
+
   //to add any new section, just add that name in contentArray and add the icon in contentIcon object
   const contentArray = [
     "about-me",
@@ -192,6 +242,7 @@ export default function Sidebar2({ content, setContent }) {
             <Typography px={2} mt={1} variant="h6" component="div" gutterBottom>
               {content.toUpperCase()}
             </Typography>
+            <MaterialUISwitch sx={{ m: 1 }} defaultChecked theme={theme} onChange={()=>setDarkMode(!darkMode)}/>
           </Toolbar>
         </AppBar>
         <Drawer variant="permanent" open={open}>
