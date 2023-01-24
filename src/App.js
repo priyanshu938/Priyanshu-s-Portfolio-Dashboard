2e8daccf7739bf0b640075b0225ab6b06af13dfa
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-01-24 22:11:55 +0530

    mui  modal files removed and login css somewhat fixed

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ import CssBaseline from "@mui/material/CssBaseline";
 import "antd/dist/reset.css";
 import { ConfigProvider, theme } from "antd";
 function App() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(true);
   return (
     <ThemeProvider
       theme={createTheme({
