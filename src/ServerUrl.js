016be66d78f092b9bbe87bb2f726fe864c4157bc
Priyanshu Tiwari <priyanshutiwari6789@gmail.com>
2023-01-31 23:38:23 +0530

    server url changed

--- src/ServerUrl.js
+++ src/ServerUrl.js
@@ -1,3 +1,4 @@
-const url="https://priyanshu-portfolio-backend.up.railway.app"
+const url = "https://priyanshu-portfolio-backend.onrender.com";
+// const url="https://priyanshu-portfolio-backend.up.railway.app"
 // const url = "http://localhost:8080";
 export default url;
